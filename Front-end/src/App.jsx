779d41aad73257d4b44a2bf3082058a5438f27db
TauraNam <tauranamirskaite@gmail.com>
2025-03-16 21:49:57 +0200

    Added components to App.jsx

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,22 @@
 import CitiesDropdown from "./components/CitiesDropdown"
+import CurrentWeatherCard from "./components/CurrentWeatherCard"
+import FiveDayForecast from "./components/FiveDayForecast"
 import { CitiesProvider } from "./context/CitiesContext"
+import { ForecastProvider } from "./context/ForecastContext"
+import { SelectedCitiesProvider } from "./context/SelectedCityContext"
 
 function App() {
 
   return (
     <>
       <CitiesProvider>
-        <CitiesDropdown />
+        <SelectedCitiesProvider>
+          <ForecastProvider>
+          <CitiesDropdown />
+          <CurrentWeatherCard />
+          <FiveDayForecast />
+          </ForecastProvider>
+        </SelectedCitiesProvider>
       </CitiesProvider>
     </>
   )
