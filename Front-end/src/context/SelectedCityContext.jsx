91de692ada5877de0f28b2a79252d988bab0ad1b
TauraNam <tauranamirskaite@gmail.com>
2025-03-16 21:36:56 +0200

    SelectedCity context

---
+++ Front-end/src/context/SelectedCityContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useContext } from 'react'
+
+const SelectedCityContext = createContext(null)
+
+export const SelectedCitiesProvider = ({ children }) => {
+    const [selectedCity, setSelectedCity] = React.useState('vilnius')
+
+    return (
+        <SelectedCityContext.Provider value={{ selectedCity, setSelectedCity }}>
+            {children}
+        </SelectedCityContext.Provider>
+    );
+}
+
+export const useSelectedCityContext = () => useContext(SelectedCityContext)
